20b0a8c278261c02b22f44d51c7cefb309f19679
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2018-09-28 14:21:18 +0200

    all types

---
+++ extra-ui/src/models/CloudFoundrySpace.ts
@@ -0,0 +1,9 @@
+import Item from './Item';
+import ItemLookup from '@/models/ItemLookup';
+
+export default class CloudFoundrySpace extends Item {
+
+  resolveDependencies(lookup: ItemLookup) {
+    // this.dashboard_url = `https://console.bluemix.net/apps/${this.resource_id}?region=${this.region}`;
+  }
+}
--- extra-ui/src/models/ItemFactory.ts
+++ extra-ui/src/models/ItemFactory.ts
@@ -7,6 +7,8 @@ import ResourceAlias from '@/models/ResourceAlias';
 import CloudFoundryServiceBinding from '@/models/CloudFoundryServiceBinding';
 import CloudFoundryOrganization from '@/models/CloudFoundryOrganization';
 import CloudFoundryServiceInstance from '@/models/CloudFoundryServiceInstance';
+import KubernetesCluster from '@/models/KubernetesCluster';
+import CloudFoundrySpace from '@/models/CloudFoundrySpace';
 
 export class ItemType {
   public id: string;
@@ -22,14 +24,16 @@ export class ItemType {
 }
 
 export const TYPES: ItemType[] = [
-  new ItemType('cf-application', 'Cloud Foundry App', '', () => new CloudFoundryApplication()),
-  new ItemType('cf-organization', 'Cloud Foundry Organization', '', () => new CloudFoundryOrganization()),
-  new ItemType('cf-service-binding', 'Cloud Foundry Service Binding', '', () => new CloudFoundryServiceBinding()),
-  new ItemType('cf-service-instance', 'Cloud Foundry Service Instance', '', () => new CloudFoundryServiceInstance()),
   new ItemType('resource-group', 'Resource Group', '', () => new ResourceGroup()),
   new ItemType('resource-instance', 'Resource Instance', '', () => new ResourceInstance()),
   new ItemType('resource-alias', 'Resource Alias', '', () => new ResourceAlias()),
   new ItemType('resource-binding', 'Resource Binding', '', () => new ResourceBinding()),
+  new ItemType('k8-cluster', 'Kubernetes Cluster', '', () => new KubernetesCluster()),
+  new ItemType('cf-application', 'Cloud Foundry App', '', () => new CloudFoundryApplication()),
+  new ItemType('cf-organization', 'Cloud Foundry Organization', '', () => new CloudFoundryOrganization()),
+  new ItemType('cf-space', 'Cloud Foundry Space', '', () => new CloudFoundrySpace()),
+  new ItemType('cf-service-binding', 'Cloud Foundry Service Binding', '', () => new CloudFoundryServiceBinding()),
+  new ItemType('cf-service-instance', 'Cloud Foundry Service Instance', '', () => new CloudFoundryServiceInstance()),
 ];
 
 const TYPESbyId: any = {};
