0d85b80d77cdfd8a074ba89ae86d2e495007521c
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2018-09-28 15:23:53 +0200

    number the releases

--- extra-plugin/build.sh
+++ extra-plugin/build.sh
@@ -15,21 +15,20 @@ echo "Synchronizing dependencies"
 cd src/extra
 govendor sync
 
-# MAJOR=$(cat extra.go | grep Major | awk '{print $2}' | tr -d ,)
-# MINOR=$(cat extra.go | grep Minor | awk '{print $2}' | tr -d ,)
-# BUILD=$(cat extra.go | grep Build | awk '{print $2}' | tr -d ,)
+MAJOR=$(cat extra.go | grep Major | awk '{print $2}' | tr -d ,)
+MINOR=$(cat extra.go | grep Minor | awk '{print $2}' | tr -d ,)
+BUILD=$(cat extra.go | grep Build | awk '{print $2}' | tr -d ,)
 
 function build {
-  FILENAME=extra-$1-$2
+  FILENAME=extra-$1-$2-$MAJOR.$MINOR.$BUILD
   echo "Building ${FILENAME}"
   GOOS=$1 GOARCH=$2 packr build -o build/$FILENAME
-#  GOOS=$1 GOARCH=$2 go build -o build/extra-$1-$2
 }
 
 # https://www.digitalocean.com/community/tutorials/how-to-build-go-executables-for-multiple-platforms-on-ubuntu-16-04
 build linux amd64
 build darwin amd64
-# build windows amd64
-# build windows 386
-# build linux 386
-# build linux ppc64
+build windows amd64
+#build windows 386
+#build linux 386
+#build linux ppc64
