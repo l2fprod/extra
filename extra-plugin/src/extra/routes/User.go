662171235fc2db50708dfc83ea54deb84bad4ea3
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2018-09-28 14:18:04 +0200

    remove timestamp from log

--- extra-plugin/src/extra/routes/User.go
+++ extra-plugin/src/extra/routes/User.go
@@ -3,25 +3,24 @@ package routes
 import (
 	"encoding/json"
 	"extra/model"
-	"log"
 	"net/http"
 
 	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/plugin"
 	"github.com/gorilla/mux"
 )
 
-type User struct {
+type user struct {
 	context plugin.PluginContext
 	router  *mux.Router
 }
 
 func RegisterUser(context plugin.PluginContext, router *mux.Router) {
-	log.Println("User API")
-	user := User{context: context, router: router}
+	// log.Println("User API")
+	user := user{context: context, router: router}
 	router.HandleFunc("/api/user", user.get)
 }
 
-func (user *User) get(w http.ResponseWriter, r *http.Request) {
+func (user *user) get(w http.ResponseWriter, r *http.Request) {
 	response := model.User{Email: user.context.UserEmail()}
 	json.NewEncoder(w).Encode(response)
 }
