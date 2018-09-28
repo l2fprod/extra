5edc06852c22085a86d127b7c56f624432e54bc0
Frederic Lavigne <fred@L2FProd.com>
2018-09-29 00:36:18 +0200

    local dev

--- extra-ui/vue.config.js
+++ extra-ui/vue.config.js
@@ -7,5 +7,15 @@ module.exports = {
         `
       }
     }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: `http://0.0.0.0:${process.env.EXTRA_PORT}`
+      },
+      '/db': {
+        target: `http://0.0.0.0:${process.env.EXTRA_PORT}`
+      }
+    }
   }
 };
