82e6aed460819367d963cc3668a8c23ba232507c
Frederic Lavigne <fred@L2FProd.com>
2018-09-29 00:11:00 +0200

    accept space, services, apps

--- extra-ui/src/models/filters/CloudFoundryOrganizationFilter.ts
+++ extra-ui/src/models/filters/CloudFoundryOrganizationFilter.ts
@@ -1,6 +1,9 @@
 import Filter from '@/models/filters/Filter';
 import Item from '@/models/Item';
 
+/**
+ * Accepts all items within a given organization (space or services or apps)
+ */
 export default class CloudFoundryOrganizationFilter implements Filter {
 
   private organization: string;
@@ -10,6 +13,10 @@ export default class CloudFoundryOrganizationFilter implements Filter {
   }
 
   public accept(item: Item): boolean {
+    if (item.parent != null && item.parent.type === 'cf-space') {
+      return this.accept(item.parent);
+    }
+
     return item.parent != null
       && item.parent.type === 'cf-organization'
       &&  this.organization === item.parent.name;
