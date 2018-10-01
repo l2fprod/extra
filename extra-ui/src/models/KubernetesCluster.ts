45ed97b4135f59e7aa85e9c0dccb336c7766ff25
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2018-10-01 11:05:26 +0200

    resolve parent and url

--- extra-ui/src/models/KubernetesCluster.ts
+++ extra-ui/src/models/KubernetesCluster.ts
@@ -1,9 +1,24 @@
 import Item from './Item';
 import ItemLookup from '@/models/ItemLookup';
+import ItemDoc from '@/models/ItemDoc';
+import ResourceInstance from '@/models/ResourceInstance';
 
-export default class KubernetesCluster extends Item {
+class Doc extends ItemDoc {
+  public resource_group_id?: string;
+}
+
+export default class KubernetesCluster extends ResourceInstance {
+
+  public doc?: Doc;
 
   public resolveDependencies(lookup: ItemLookup) {
-//    this.dashboard_url = `https://console.bluemix.net/apps/${this.resource_id}?region=${this.region}`;
+    this.dashboard_url = 'https://console.bluemix.net/containers-kubernetes/clusters/' +
+      `${this.service_instance}/overview?region=${this.region}`;
+
+    if (this.doc && this.doc.resource_group_id) {
+      this.resourceGroup = lookup.findByResourceId(this.doc!.resource_group_id!);
+      this.parent = this.resourceGroup;
+    }
   }
+
 }
