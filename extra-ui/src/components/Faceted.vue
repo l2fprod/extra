798a7441b8dceee4ce3106d947eaf7722793534a
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2018-10-09 10:13:28 +0200

    filter on space

--- extra-ui/src/components/Faceted.vue
+++ extra-ui/src/components/Faceted.vue
@@ -22,15 +22,26 @@
           <span class="filter-title" v-bind:class="{'has-filter-selected': selectedOrgs.length > 0}">Cloud Foundry</span>
         </div>
         <v-list dense>
-          <v-list-tile v-for="organization in $store.getters.organizations" v-bind:key="organization">
-            <v-list-tile-action>
-              <v-checkbox v-model="selectedOrgs" :value="organization" @change="updateFilter">
-                <span class="filter-item" slot="label">{{organization}}
-                  <span class="filter-count font-weight-thin"> ({{ countInOrganization(organization) }})</span>
-                </span>
-              </v-checkbox>
-            </v-list-tile-action>
-          </v-list-tile>
+          <template v-for="organization in $store.getters.organizations">
+            <v-list-tile  v-bind:key="organization">
+              <v-list-tile-action>
+                <v-checkbox v-model="selectedOrgs" :value="organization" @change="updateFilter">
+                  <span class="filter-item" slot="label">{{organization}}
+                    <span class="filter-count font-weight-thin"> ({{ countInOrganization(organization) }})</span>
+                  </span>
+                </v-checkbox>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-list-tile class="nested-item" v-for="space in $store.getters.spaces(organization)" v-bind:key="organization + '/' + space">
+              <v-list-tile-action>
+                <v-checkbox v-model="selectedSpaces[organization + '\t' + space]" @change="updateFilter">
+                  <span class="filter-item" slot="label">{{space}}
+                    <span class="filter-count font-weight-thin"> ({{ countInSpace(organization, space) }})</span>
+                  </span>
+                </v-checkbox>
+              </v-list-tile-action>
+            </v-list-tile>
+          </template>
         </v-list>
       </v-expansion-panel-content>
       <v-expansion-panel-content>
@@ -77,11 +88,16 @@ import ResourceGroupFilter from '@/models/filters/ResourceGroupFilter';
 import TypeFilter from '@/models/filters/TypeFilter';
 import RegionFilter from '@/models/filters/RegionFilter';
 import CloudFoundryOrganizationFilter from '@/models/filters/CloudFoundryOrganizationFilter';
+import CloudFoundrySpaceFilter from '@/models/filters/CloudFoundrySpaceFilter';
 import AndFilter from '@/models/filters/AndFilter';
 import OrFilter from '@/models/filters/OrFilter';
 import NotFilter from '@/models/filters/NotFilter';
 import Item from '@/models/Item';
 import Filter from '@/models/filters/Filter';
+
+interface SpaceNameToBoolean {
+  [key: string]: boolean;
+}
 
 @Component
 export default class Faceted extends Vue {
@@ -91,10 +107,12 @@ export default class Faceted extends Vue {
       '--secondary-color': this.$vuetify.theme.secondary,
     };
   }
+  
   public selectedGroups: string[] = [];
   public selectedTypes: string[] = [];
   public selectedRegions: string[] = [];
   public selectedOrgs: string[] = [];
+  public selectedSpaces: SpaceNameToBoolean = {};
   private types = TYPES;
 
   public countInGroup(group: ResourceGroup) {
@@ -113,6 +131,14 @@ export default class Faceted extends Vue {
     return this.$store.getters.filteredResources(new TypeFilter(type.id)).length;
   }
 
+  public countInSpace(organization: string, space: string) {
+    return this.$store.getters.filteredResources(
+      new AndFilter([
+        new CloudFoundryOrganizationFilter(organization),
+        new CloudFoundrySpaceFilter(space),
+    ])).length;
+  }
+
   public resetFilter() {
     this.selectedGroups = [];
     this.selectedTypes = [];
@@ -127,11 +153,22 @@ export default class Faceted extends Vue {
     const regionsFilters = this.selectedRegions.map((region) => new RegionFilter(region));
     const organizationsFilters =
       this.selectedOrgs.map((organization) => new CloudFoundryOrganizationFilter(organization));
+    const spaceFilters: Filter[] = [];
+    Object.keys(this.selectedSpaces).forEach((orgAndSpace:string) => {
+      if (this.selectedSpaces[orgAndSpace]) {
+        const values = orgAndSpace.split('\t');
+        spaceFilters.push(new AndFilter([
+          new CloudFoundryOrganizationFilter(values[0]),
+          new CloudFoundrySpaceFilter(values[1]),
+        ]));
+      }
+    });
     const filter = new AndFilter([
       new OrFilter(groupFilters),
       new OrFilter(typesFilters),
       new OrFilter(regionsFilters),
       new OrFilter(organizationsFilters),
+      new OrFilter(spaceFilters),
     ]);
     this.$store.dispatch('filter', filter);
   }
@@ -164,4 +201,7 @@ export default class Faceted extends Vue {
   text-align: left;
 }
 
+.nested-item {
+  margin-left: 20px;
+}
 </style>
